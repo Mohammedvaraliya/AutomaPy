b0e3b5fce1323179065c0fc5525e1a6ef4367b41
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-03-20 23:42:41 +0530

    Add Turing Machine Code

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_description():
         return contents.strip()
     
 
-VERSION = '1.3.1'
+VERSION = '1.3.0'
 DESCRIPTION = 'This package refers to the topic of automata theory, which includes DFA, NDFA, Mealy machines, Moore machines, Finite state machine and Turing machine.'
 
 # Setting up
